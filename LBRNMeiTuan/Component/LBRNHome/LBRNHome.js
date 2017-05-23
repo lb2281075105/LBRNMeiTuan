07d59de442427284403b1e1c7ca63a0fe96a42f9
CodeJCSON <2334497007@qq.com>
2017-05-23 17:15:31 +0800

    首页导航栏设置

--- LBRNMeiTuan/Component/LBRNHome/LBRNHome.js
+++ LBRNMeiTuan/Component/LBRNHome/LBRNHome.js
@@ -10,7 +10,11 @@ import {
     StyleSheet,
     Text,
     View,
-    Platform
+    Platform,
+    Image,
+    TextInput,
+    TouchableOpacity,
+    AlertIOS
 } from 'react-native';
 
 var width = require('Dimensions').get('window').width;
@@ -18,12 +22,28 @@ var LBRNHome = React.createClass({
         render(){
             return (
                 <View style={styles.container}>
-                    <View style={styles.homeStyleStyle}>
-                        <Text style={{color: 'white',fontSize:18,marginTop:28,fontWeight:'bold'}}>首页</Text>
-                    </View>
-
+                    {/*导航栏设置*/}
+                    {this.renderHomeNavbar()}
                 </View>
             );
+        },
+        renderHomeNavbar(){
+            return(
+                <View style={styles.homeStyleStyle}>
+                    <TouchableOpacity onPress={()=>{AlertIOS.alert('济南')}}>
+                        <Text style={{color:'white',fontSize:16,marginLeft:10,fontWeight:'bold'}}>济南</Text>
+                    </TouchableOpacity>
+
+                    <TextInput style={styles.textInputStyle}
+                               placeholder={'输入商家，品类，商圈'}
+                               placeholderTextColor={'#bbb'}
+                               underlineColorAndroid='white'
+                               returnKeyType='search'
+                    />
+                    <Image source={{uri:'icon_homepage_message'}} style={[styles.toolBarIcon,{marginTop:3}]}/>
+                    <Image source={{uri:'icon_homepage_scan'}} style={[styles.toolBarIcon,{marginRight:10,marginTop:5}]}/>
+                </View>
+            )
         }
     }
 )
@@ -38,7 +58,21 @@ const styles = StyleSheet.create({
         width:width,
         height:Platform.OS === 'ios' ? 64 : 44,
         backgroundColor:'#FB6320',
+        flexDirection:'row',
+        justifyContent:'space-between',
         alignItems:'center'
+    },
+    textInputStyle:{
+        width:width*0.6,
+        height:35,
+        backgroundColor:'white',
+        borderRadius:18,
+        paddingLeft:14,
+        marginTop:18
+    },
+    toolBarIcon:{
+        width:30,
+        height:30
     }
 });
 
