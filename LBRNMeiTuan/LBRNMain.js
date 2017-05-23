8753df70b9c779a53efe052eec66d1c4ffa20ffd
CodeJCSON <2334497007@qq.com>
2017-05-23 15:43:05 +0800

    "我的"、"更多"模块的完成(其实是一个类型)

---
+++ LBRNMeiTuan/LBRNMain.js
@@ -0,0 +1,87 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Navigator
+} from 'react-native';
+import TabNavigator from 'react-native-tab-navigator';
+
+//获取首页,电商,我的,更多文件
+var LBRNHome = require('./Component/LBRNHome/LBRNHome');
+var LBRNEB = require('./Component/LBRNEB/LBRNEB');
+var LBRNMine = require('./Component/LBRNMine/LBRNMine');
+var LBRNMore = require('./Component/LBRNMore/LBRNMore');
+
+var LBRNMain = React.createClass({
+
+    getInitialState(){
+       return {
+            selectedTab:'Home'
+      }
+    },
+    render() {
+        return (
+            <TabNavigator>
+
+                {this.childNavigetor('首页','Home','icon_tabbar_homepage','icon_tabbar_homepage_selected','Home',LBRNHome)}
+                {this.childNavigetor('商家','EB','icon_tabbar_merchant_normal','icon_tabbar_merchant_selected','EB',LBRNEB)}
+                {this.childNavigetor('我的','Mine','icon_tabbar_mine','icon_tabbar_mine_selected','Mine',LBRNMine)}
+                {this.childNavigetor('更多','More','icon_tabbar_misc','icon_tabbar_misc_selected','More',LBRNMore)}
+            </TabNavigator>
+        );
+    },
+
+    childNavigetor(title,tabName,normalImage, selectedImage,componentName,component){
+        return (
+            <TabNavigator.Item
+                selectedTitleStyle={styles.selectedTextStyle}
+                selected={this.state.selectedTab === tabName}
+                title={title}
+                renderIcon={() => <Image source={{uri:normalImage}} style={styles.iconStyle} />}
+                renderSelectedIcon={() => <Image source={{uri:selectedImage}} style={styles.iconStyle} />}
+                onPress={() => this.setState({ selectedTab: componentName })}>
+                <Navigator
+                    initialRoute={{ name: componentName, component: component }}
+                    configureScene={(route) => {
+                        return Navigator.SceneConfigs.PushFromRight;
+                    }}
+                    renderScene={(route, navigator) => {
+                        let Component = route.component;
+                        return <Component {...route.props} navigator={navigator} />
+                    }}
+                />
+
+            </TabNavigator.Item>
+        )
+    }
+})
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    iconStyle:{
+        width:28,
+        height:28
+    },
+    selectedTextStyle:{
+        color:'#fb6320'
+
+    }
+
+
+});
+
+module.exports = LBRNMain;
---
+++ LBRNMeiTuan/README.md
@@ -0,0 +1,28 @@
+# React Native MeiTuan
+MeiTuan client for react native
+# Demo
+- Android:[Download Apk](https://github.com/githubhaohao/MeiTuan/raw/master/gif/meituan.apk)
+- iOS:TBD
+# Set up 
+1. git clone https://github.com/githubhaohao/MeiTuan.git
+2. cd MeiTuan
+3. npm install
+4. react native run-android
+# Gif
+![img1](https://github.com/githubhaohao/MeiTuan/blob/master/gif/img1.gif)
+![img2](https://github.com/githubhaohao/MeiTuan/blob/master/gif/img2.gif)
+![img3](https://github.com/githubhaohao/MeiTuan/blob/master/gif/img3.gif)
+# License
+Copyright 2016 (c) GithubHaohao
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
